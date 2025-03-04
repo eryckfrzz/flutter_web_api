0956bc13d945a5d7b3186ac2684487c7a0638729
Kaio <kaioericak@gmail.com>
2025-03-04 13:53:38 -0300

    inicialização do projeto juntamente com a configuração do tema e fontes.

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.white),
+          elevation: 0,
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      initialRoute: "home",
+      routes: {"home": (context) => const HomeScreen()},
+    );
+  }
+}
